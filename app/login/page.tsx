fba3374bbe565e51eee3331459fb6f5348559094
JaydethSp01 <132963442+JaydethSp01@users.noreply.github.com>
2026-06-07 23:44:02 -0500

    feat: ScrumDev AI build :: app/login/page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+export const dynamic = "force-dynamic";
+import { LoginForm } from "@/components/ui/LoginForm";
+
+export default function LoginPage() {
+  return <LoginForm appName="Veterinaria" />;
+}
